772a8fbb76d34a6930024c0a18e8782f2f4c9ebe
deevee <github@vansteenwegen.org>
2021-09-30 22:36:06 +0200

    Added type hints and docstrings

--- photo_renamer.py
+++ photo_renamer.py
@@ -1,28 +1,36 @@
 from datetime import datetime
 import os
 import shutil
+from typing import Tuple, Union
 from exif import Image
 
 DOUBLES_FILENAME = 'doubles.list'
 
 
-
 class PhotoRenamer:
-    def __init__(self, path, file, logger = None):
+    def __init__(self, src_path, file, logger):
         self.logger = logger
-        self.path = path
+        self.path = src_path
         self.src_file = file
 
-    def rename_if_jpeg(self):
-        if not self.src_file[-5:].lower() == '.jpeg':
+    def rename_if_jpeg(self) -> None:
+        """If file exention is .jpeg, rename to .jpg and update self.src_file"""
+        if self.src_file[-5:].lower() != '.jpeg':
             return
         jpg_filename = self.src_file[:-5] + '.jpg'
         if self.logger:
             self.logger.info('Renaming "{}" to "{}"'.format(self.src_file, jpg_filename))
-        os.rename(os.path.join(self.path, self.src_file), os.path.join(self.path,jpg_filename))
+        os.rename(os.path.join(self.path, self.src_file), os.path.join(self.path, jpg_filename))
         self.src_file = jpg_filename
 
-    def get_exif_data(self):
+    def get_exif_data(self) -> bool:
+        """Populate properties from exif data.
+
+        Populate self.dt, self.dt_orig, self.model
+
+        Returns:
+            bool: True is self.dt or self.dt_orig has succesfully been parsed.
+        """
         with open(os.path.join(self.path, self.src_file), 'rb') as src_file:
             exif_src = Image(src_file)
 
@@ -59,14 +67,15 @@ class PhotoRenamer:
             msg += ','.join(issues)
             self.logger.info(msg)
 
-        return True if self.dt or self.dt_orig else False
+        return bool(self.dt or self.dt_orig)
 
-    def check_whatsapp(self):
+    def check_whatsapp(self) -> Union[bool, None]:
         """ Check if filename matches whatsapp pattern and extract date and set number.
         example filename: IMG-20181108-WA0025
         Returns True succesful
         Returns False if not
-        TODO: maybe files can have a WA filename with correct original date, but have exif data that doesn't match.
+        TODO: maybe files can have a WA filename with correct original date,
+                but have exif data that doesn't match.
         TODO: If that is possible, check first if whatsapp image, then overwrite exif dt_orig.
         """
         if self.src_file[:4] == 'IMG-':
@@ -74,14 +83,17 @@ class PhotoRenamer:
                 without_ext = os.path.splitext(self.src_file)[0]
                 self.dt = datetime.strptime(without_ext[4:12], '%Y%m%d')
                 number = int(without_ext[-4:])
-                self.dt = self.dt.replace(minute = number // 60, second = number %60)
+                self.dt = self.dt.replace(minute=number // 60, second=number % 60)
 
-                return True
-            except:
+            except Exception:
                 return False
 
+            else:
+                return True
+        else:
+            return None
 
-    def dt_matches_dt_orig(self):
+    def dt_matches_dt_orig(self) -> bool:
         """ check if both datetimes are filled in and match.
         Return True if only one is filled in, or if both match. Else return False.
         """
@@ -89,45 +101,94 @@ class PhotoRenamer:
             return True
         return self.dt == self.dt_orig
 
+    def new_filename(self,
+                     target_path: str,
+                     include_cam_model: bool = True,
+                     keywords_to_keep: Union[str, None] = None,
+                     replace_chars_in_model: Union[None, list[str]] = None) -> Tuple[str, str]:
+        """Generate new filename based on target path and picture properties
 
-    def new_filename(self, include_camera_model = True, keywords_to_keep = None, replace_chars_in_model = None):
+        Args:
+            target_path (str): Base path for target
+            include_cam_model (bool, optional): Add camera model (if available)to filename. 
+                                                Defaults to True.
+            keywords_to_keep (Union[str, None], optional): Certain keywords that should be kept if
+                                                           they are in orig filename (BURST,...)
+                                                           Defaults to None.
+            replace_chars_in_model (Union[None, list[str]], optional): List of lists containing . Defaults to None.
+
+        Returns:
+            Tuple[str, str]: target path, filename
+        """
         new_filename = ''
-        date_source = self.dt_orig if self.dt_orig else self.dt  # prefer dt_orig...
+        date_source = self.dt_orig or self.dt
         new_filename += date_source.strftime('%Y%m%d_%H%M%S')
 
-        for keyword in keywords_to_keep:
-            if keyword in self.src_file:
-                new_filename += '_' + keyword
+        if keywords_to_keep:
+            for keyword in keywords_to_keep:
+                if keyword in self.src_file:
+                    new_filename += '_' + keyword
 
-        if include_camera_model and self.model:
-            for i,j in replace_chars_in_model:
-                    self.model = self.model.replace(i, j)
+        if include_cam_model and self.model and replace_chars_in_model:
+            for i, j in replace_chars_in_model:
+                self.model = self.model.replace(i, j)
             new_filename += '(_{})'.format(self.model)
 
         new_filename += '.jpg'
         year = str(date_source.year)
         month = ('{:02d}'.format(date_source.month))
-        target_dir = os.path.join(self.path, year, month)
+        target_dir = os.path.join(target_path, year, month)
         return target_dir, new_filename.upper()
 
-    def move_file(self, src_file, new_path, new_filename, process_doubles = True):
-        is_double = False
+    def move_file(self,
+                  src_file: str,
+                  new_path: str,
+                  new_filename: str,
+                  process_doubles: bool = True) -> Tuple[Union[str, None], bool]:
+        """Move source file to new path and filename.
+
+        Args:
+            src_file (str): Original path+filename
+            new_path (str): Target path
+            new_filename (str): target filename
+            process_doubles (bool, optional): If target already exists, generate new filename. 
+                                              Defaults to True.
+
+        Returns:
+            Tuple[Union[str,None], bool]: filename if new file was moved (None if not),
+                                          is a double
+        """
+
         ideal_target_file = os.path.join(new_path, new_filename)
         target_file = self.generate_unique_filename(ideal_target_file, process_doubles)
-        if not target_file == ideal_target_file:
-            is_double = True
+        is_double = (target_file != ideal_target_file)
         if target_file:
             if not os.path.isdir(new_path):
                 os.makedirs(new_path)
             return (shutil.move(src_file, target_file), is_double)
         else:
             return (None, is_double)
-
-    def generate_unique_filename(self, base_filename, process_doubles):
-        """ generate unique filename (if required)
-        checks if base_filename already exists. 
+        """Generate unique filename (if required)
+        checks if base_filename already exists.
         if not, return base_filename.
-        If it exists, return a newly generated filename if process_doubles is enables
+        If it exists, return a newly generated filename if process_doubles is enabled
+        """
+
+    def generate_unique_filename(self, base_filename: str,
+                                 process_doubles: bool) -> Union[str, None]:
+        """Generate a unique target file name
+
+        If target file name already exists, add underscore and counter until target filename is
+        a unique filename.
+
+        Args:
+            base_filename (str): original target filename
+            process_doubles (bool): proceed with generating alternative filenames if 
+                                    target already exists
+
+        Returns:
+            Union[str, None]: None if target exists but process_doubles is True
+                              str if a new, unique filename has been generated
         """
         if not os.path.isfile(base_filename):
             return base_filename
@@ -138,8 +199,8 @@ class PhotoRenamer:
             return None
         counter = 1
         filename, ext = base_filename.rsplit('.', 1)
-        new_filename = filename+ '_COPY[{}]' + '.' + ext
+        new_filename = filename + '_COPY[{}]' + '.' + ext
         while os.path.exists(new_filename.format(counter)):
-            counter +=1
+            counter += 1
         self.logger.info('Double filename: {}'.format(new_filename.format(counter)))
         return new_filename.format(counter)
--- sort_them.py
+++ sort_them.py
@@ -1,49 +1,77 @@
-# from plum._plum import UnpackError
 import logging, logging.handlers
 import os
 import threading
 from time import sleep
+from typing import Iterator, List, Tuple
 from photo_renamer import PhotoRenamer as pr
 from datetime import datetime
 from fnmatch import fnmatch as filename_match
 
-IMAGE_DIR = r'G:\temp2\Motorola moto g(8) plus'
-INCLUDE_CAMERA_MODEL = True
-REPLACE_CHARS_IN_MODEL=(('_','-'),('(',''),(')',''),(' ','-'))
+INCLUDE_CAMERA_MODEL: bool = True
+REPLACE_CHARS_IN_MODEL = (('_', '-'), ('(', ''), (')', ''), (' ', '-'))
 KEYWORDS_TO_KEEP = ['HDR', 'PORTRAIT', 'WA', 'BURST', 'COVER', 'TOP']
-PROCESS_DOUBLES = True  # if a picture with same datetime exist, if True: rename, if False: don't touch/process original
-DT_AND_DT_ORIG_NEED_TO_MATCH = False
+PROCESS_DOUBLES: bool = True  # if a picture with same datetime exist: True: rename, else: don't process original
+DT_AND_DT_ORIG_NEED_TO_MATCH: bool = False
+# IMAGE_DIR = 'P:\Automatic Upload\Motorola moto g(8) plus'
+SOURCE_PATH = 'P:\\Automatic Upload\\temp'
+TARGET_PATH = 'P:\\fotos\\g8_sorted'
 
-MSG_PROCESSED = 'Processed: {0[processed]}, moved: {0[moved]}, double: {0[double]}, no dt: {0[no_dt]}, whatsapp: {0[whatsapp]}, dt_mismatch: {0[dt_mismatch]}'
+MSG_PROCESSED = ('Processed: {0[processed]}, moved: {0[moved]}, double: {0[double]}, '
+                 'no dt: {0[no_dt]}, whatsapp: {0[whatsapp]}, dt_mismatch: {0[dt_mismatch]}')
 LOG_FILENAME = 'output.log'
 DT_FORMAT = '%d/%m/%Y %H:%M:%S'
 LOG_MSG_FORMAT = '%(asctime)s|%(levelname)s|%(message)s'
 LOG_NAME = 'PhotoRenamer'
-files = {'processed':0, 'moved': 0, 'double':0, 'no_dt':0, 'whatsapp':0, 'dt_mismatch': 0}
+files = {'processed': 0, 'moved': 0, 'double': 0, 'no_dt': 0, 'whatsapp': 0, 'dt_mismatch': 0}
 
 
 def create_logger():
-    rf_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1000000, backupCount = 5)
+    rf_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
+                                                      maxBytes=1000000,
+                                                      backupCount=5)
     formatter = logging.Formatter(LOG_MSG_FORMAT, DT_FORMAT)
     rf_handler.setFormatter(formatter)
     logger = logging.getLogger(LOG_NAME)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(rf_handler)
     logger.info('---Init logger---')
-    logger.debug('Base directory: "{}"'.format(abs_path))
+    logger.debug('Base directory: "{}"'.format(abs_src_path))
     return logger
 
-def find_files(source_dir, pattern, logger = None, e = None):
+
+def find_files(source_dir: str,
+               pattern: Tuple[str, ...],
+               logger: logging.Logger,
+               e: threading.Event = None) -> Iterator[str]:
+    """Generate a list of picture files and yield one by one.
+
+    Args:
+        source_dir (str): Source dir to scan for pictures
+        pattern (Tuple[str, ...]): Filename pattern to search for (i.e. '*.jpg')
+        logger (logging.Logger): Where to log to.
+        e (threading.Event, optional): To signal that list has been created. Defaults to None.
+
+    Yields:
+        str: file in (subdir of) source_dir matching the pattern.
+    """
     try:
-        msg = 'creating file list, this might take a while.'
-        if logger: logger.debug(msg)
-        files = [file for file in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir,file)) and check_multiple_patterns(file, pattern)]
-        msg = 'done creating file list, total of {} files in {}'.format(len(files), source_dir)
+        logger.debug('Creating file list, this might take a while.')
+
+        all_files_full = [
+            file for file in os.listdir(source_dir)
+            if os.path.isfile(os.path.join(source_dir, file))
+        ]  # all files (full path) in (subdirs of) source_dir
+        files_full = [file for file in all_files_full if check_multiple_patterns(file, pattern)]
+
+        msg = f'Done creating file list, total of {len(files)} files in {source_dir}'
         print(msg)
-        if logger: logger.debug(msg)
+        logger.debug(msg)
+
         if e:
             e.set()
+
         yield from files
+
     except FileNotFoundError:
         import sys
         msg = 'FileNotFoundError: No files found, does directory exist? Exiting...'
@@ -51,22 +79,45 @@ def find_files(source_dir, pattern, logger = None, e = None):
         logger.error(msg)
         sys.exit()
 
-def check_multiple_patterns(file, patterns):
-    return any(filename_match(file,pattern) for pattern in patterns)
+
+def check_multiple_patterns(file_full: str, patterns: Tuple[str, ...]) -> bool:
+    """Check if the filename matches one of the patterns in patterns
+
+    Args:
+        file_full (str): Full path of the file to check
+        patterns (List[str]): List of patterns to check for
+
+    Returns:
+        bool: True if filename matches one of the patterns
+    """
+    return any(filename_match(file_full, pattern) for pattern in patterns)
 
 
-def print_results():
+def print_results() -> None:
+    """Print actions and time taken."""
     total_time = datetime.now() - start_time
-    total_time_str = '{} minutes, {} seconds'.format(total_time.seconds//60, total_time.seconds%60)
-    msg_time = 'Total time: ' + total_time_str
+    total_time_str = '{} minutes, {} seconds'.format(total_time.seconds // 60,
+                                                     total_time.seconds % 60)
+
     print('Finished.')
     logger.info('Finished.')
+
     print(MSG_PROCESSED.format(files))
     logger.info(MSG_PROCESSED.format(files))
+
+    msg_time = 'Total time: ' + total_time_str
     print(msg_time)
     logger.info(msg_time)
 
-def print_info(e = None):
+
+def print_info(e: threading.Event = None) -> None:
+    """Print info to stdout
+
+    Print either that files are being listed of the current status.
+
+    Args:
+        e (threading.Event, optional): signal that file list has been made. Defaults to None.
+    """
     while True:
         cur_time = datetime.now().strftime(DT_FORMAT)
         if e and not e.is_set():
@@ -75,40 +126,57 @@ def print_info(e = None):
             print(cur_time + ' ' + MSG_PROCESSED.format(files))
         sleep(5)
 
-def main_operation(abs_path, logger, e = None):
+
+def main_operation(abs_src_path: str, logger: logging.Logger, e: threading.Event = None):
+    """List all files to be checked, then go over them and move if necessary.
+
+    Args:
+        abs_src_path (str): Absolute source directory (where files currently are)
+        logger (logging.Logger): where to log to
+        e (threading.Event, optional): [description]. Defaults to None.
+    """
     global files
 
-    for file in find_files(abs_path, ('*.jpeg', '*.jpg'), logger = logger, e= e):
-        jpg = pr(path=abs_path, file=file, logger=logger)
+    for file in find_files(abs_src_path, ('*.jpeg', '*.jpg'), logger=logger, e=e):
+        jpg = pr(src_path=abs_src_path, file=file, logger=logger)
         files['processed'] += 1
         logger.debug('Processing "{}"'.format(file))
 
         if not jpg.get_exif_data():
             files['no_dt'] += 1
             if jpg.check_whatsapp():
-                files['whatsapp'] +=1
+                files['whatsapp'] += 1
             else:
-                logger.warning('no dt(_orig) for file "{}" and not a WhatsApp picture. Skipping this file.'.format(file))
+                logger.warning(
+                    'no dt(_orig) for file "{}" and not a WhatsApp picture. Skipping this file.'.
+                    format(file))
                 continue
         if DT_AND_DT_ORIG_NEED_TO_MATCH and not jpg.dt_matches_dt_orig():
             # both are in exif data, but don't match. Might add option to keep the oldest date
-            logger.warning('dt ({}) does not match dt_orig ({}) for "{}". Skipping this file...'.format(jpg.dt, jpg.dt_orig, file))
-            files['dt_mismatch']  += 1
+            logger.warning(
+                'dt ({}) does not match dt_orig ({}) for "{}". Skipping this file...'.format(
+                    jpg.dt, jpg.dt_orig, file))
+            files['dt_mismatch'] += 1
             continue
 
-        new_path, new_filename = jpg.new_filename(INCLUDE_CAMERA_MODEL, KEYWORDS_TO_KEEP, REPLACE_CHARS_IN_MODEL)
-        rtn = jpg.move_file(os.path.join(abs_path, file), new_path, new_filename, PROCESS_DOUBLES)
+        new_path, new_filename = jpg.new_filename(INCLUDE_CAMERA_MODEL, KEYWORDS_TO_KEEP,
+                                                  REPLACE_CHARS_IN_MODEL)
+        rtn = jpg.move_file(os.path.join(abs_src_path, file), new_path, new_filename,
+                            PROCESS_DOUBLES)
         if rtn[0]:
             logger.debug('moved file {} to {}'.format(file, rtn[0]))
             files['moved'] += 1
         else:
-            logger.info('file {} is a double and has not been processed (PROCESS_DOUBLES SET TO {}'.format(file, PROCESS_DOUBLES))
+            logger.info(
+                'file {} is a double and has not been processed (PROCESS_DOUBLES SET TO {}'.format(
+                    file, PROCESS_DOUBLES))
         if rtn[1]:
-                files['double'] += 1
+            files['double'] += 1
 
 
 if __name__ == '__main__':
-    abs_path = os.path.abspath(IMAGE_DIR)
+
+    abs_src_path = os.path.abspath(SOURCE_PATH)
     logger = create_logger()
 
     start_time = datetime.now()
@@ -117,7 +185,7 @@ if __name__ == '__main__':
 
     main_thread = threading.Thread(name='main_operation', \
                                     target = main_operation, \
-                                    args=(abs_path, logger, e))
+                                    args=(abs_src_path, logger, e))
     print_thread = threading.Thread(name='print_info', \
                                         target=print_info, \
                                         args=(e,),
